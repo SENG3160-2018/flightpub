ef4409cbc60e15e30c7968074ad9ab396bdb314b
b8ne <sutt8r@gmail.com>
2018-10-09 09:10:43 +1100

    Added junit and test structures

---
+++ test/QueryFlights/Feature/QueryFlightTest.java
@@ -0,0 +1,7 @@
+package QueryFlights.Feature;
+
+import org.apache.struts2.StrutsTestCase;
+
+public class QueryFlightTest extends StrutsTestCase {
+
+}
---
+++ test/QueryFlights/Unit/QueryFlightTest.java
@@ -0,0 +1,7 @@
+package QueryFlights.Unit;
+
+import org.apache.struts2.StrutsTestCase;
+
+public class QueryFlightTest extends StrutsTestCase {
+
+}
